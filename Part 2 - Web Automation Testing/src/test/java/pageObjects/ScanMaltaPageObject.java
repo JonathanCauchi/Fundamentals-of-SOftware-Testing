364e1427c392a3e60f06dd3dbe6711d17a918504
Jonathan Cauchi <jonathan.cauchi.14@um.edu.mt>
2019-12-02 11:18:56 +0100

    started and almost completed work for part 2

---
+++ Part 2 - Web Automation Testing/src/test/java/pageObjects/ScanMaltaPageObject.java
@@ -0,0 +1,120 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ScanMaltaPageObject {
+
+    WebDriver browser = null;
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (Exception e) {}
+    }
+
+    public ScanMaltaPageObject(WebDriver browser) {
+        this.browser = browser;
+    }
+
+    public void getPage() {
+        browser.get("https://www.scanmalta.com/newstore/customer/account/login/");
+    }
+
+    public void validLogin(String username, String password) {
+        browser.findElement(By.name("login[username]")).sendKeys(username);
+        browser.findElement(By.name("login[password]")).sendKeys(password);
+        browser.findElement(By.name("send")).submit();
+        sleep(2);
+    }
+
+    public void validateLogin() {
+        assertTrue(browser.findElement(By.className("hello")).getText().contains("Hello, Anshu Saurabh!"));
+    }
+
+    public void invalidLogin(String username, String wrongPass) {
+        browser.findElement(By.name("login[username]")).sendKeys(username);
+        browser.findElement(By.name("login[password]")).sendKeys(wrongPass);
+        browser.findElement(By.name("send")).submit();
+        sleep(3);
+    }
+
+    public void validateInvalidLogin() {
+        assertTrue(browser.findElement(By.className("error-msg")).getText().contains("Invalid login or password."));
+    }
+
+    public void search(String product) {
+        browser.findElement(By.id("search")).sendKeys(product);
+        browser.findElement(By.className("icon-search")).submit();
+        sleep(2);
+    }
+
+    public void selectFirstProduct() {
+        List<WebElement> productsList = browser.findElements(By.className("item-images"));
+        WebElement firstProduct = productsList.get(0);
+        firstProduct.click();
+        sleep(2);
+    }
+
+    public void productDetailsFound() {
+        assertTrue(browser.findElement(By.className("product-description")).getText().contains("Product Description"));
+    }
+
+    public void goToCart() {
+        browser.get("https://www.scanmalta.com/newstore/checkout/cart/");
+        sleep(3);
+    }
+
+    public void emptyCart() {
+        if(getCartAmount() != 0) {
+            browser.findElement(By.id("empty_cart_button")).click();
+        }
+        sleep(2);
+    }
+
+    public void addToCart() {
+        browser.findElement(By.id("product-addtocart-button")).submit();
+        sleep(7); //due to popup that follows
+    }
+
+    public int getCartAmount() {
+        String amount = browser.findElement(By.xpath("//span[contains(text(), 'items')]")).getText();
+        return Integer.parseInt(amount.split(" ")[0]);
+    }
+
+    public void cartHasOneItem(int int1) {
+        int items = getCartAmount();
+        assertEquals(items, int1);
+    }
+
+    public void selectMultipleProductsAndAddToCart(int int1) {
+
+        int i = 0;
+
+        do {
+            search("ssd");
+            List<WebElement> productsList = browser.findElements(By.className("item-images"));
+            System.out.println("Loop No. " + i);
+            WebElement product = productsList.get(i);
+            sleep(2);
+            product.click();
+            sleep(2);
+            addToCart();
+            i++;
+        }while(i != int1);
+        
+    }
+
+    public void cartHasMultipleItems(int int1) {
+        int items = getCartAmount();
+        System.out.println("Number in cart " + items +" should contain " + int1); //test to see amount in cart and what it should have
+        assertEquals(items, int1);
+    }
+
+}
---
+++ Part 2 - Web Automation Testing/src/test/java/test/cucumber/stepdefs/ScanMaltaStepDefs.java
@@ -0,0 +1,127 @@
+package test.cucumber.stepdefs;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.ScanMaltaPageObject;
+
+import static org.junit.Assert.assertTrue;
+
+public class ScanMaltaStepDefs {
+
+    WebDriver browser = null;
+    public ScanMaltaPageObject sut;
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (Exception e) {}
+    }
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "D:/anshu/Downloads/chromedriver.exe");
+        browser = new ChromeDriver();
+        sut = new ScanMaltaPageObject(browser);
+    }
+
+    @After
+    public void teardown() {
+        browser.quit();
+    }
+
+    //1
+    @Given("I am a user on the website")
+    public void i_am_a_user_on_the_website() {
+        sut.getPage();
+        //browser.get("https://www.scanmalta.com/newstore/customer/account/login/");
+        //sleep(5);
+    }
+
+    @When("I log in using valid credentials {string} and {string}")
+    public void i_log_in_using_valid_credentials(String username, String password) {
+        sut.validLogin(username, password);
+    }
+
+    @Then("I should be logged in")
+    public void i_should_be_logged_in() {
+        sut.validateLogin();
+    }
+
+    //2
+    @When("I log in using invalid credentials {string} and {string}")
+    public void i_log_in_using_invalid_credentials_and(String username, String wrongPass) {
+        sut.invalidLogin(username, wrongPass);
+    }
+
+    @Then("I should not be logged in")
+    public void i_should_not_be_logged_in() {
+        //assertTrue(browser.findElement(By.className("error-msg")).getText().contains("Invalid login or password."));
+        sut.validateInvalidLogin();
+    }
+
+    //3
+    @Given("I am a logged in user on the website {string} and {string}")
+    public void i_am_a_logged_in_user_on_the_website(String username, String password) {
+        sut.getPage();
+        sut.validLogin(username, password);
+    }
+
+    @When("I search for a product {string}")
+    public void i_search_for_a_product(String product) {
+        sut.search(product);
+    }
+
+    @When("I select the first product in the list")
+    public void i_select_the_first_product_in_the_list() {
+        sut.selectFirstProduct();
+    }
+
+    @Then("I should see the product details")
+    public void i_should_see_the_product_details() {
+        sut.productDetailsFound();
+    }
+
+
+    @Given("my shopping cart is empty")
+    public void my_shopping_cart_is_empty() {
+        sut.goToCart();
+        sut.emptyCart();
+    }
+
+    @When("I view the details of a product {string}")
+    public void i_view_the_details_of_a_product(String product) {
+        sut.search(product);
+        sut.selectFirstProduct();
+    }
+
+    @When("I choose to buy the product")
+    public void i_choose_to_buy_the_product() {
+        sut.addToCart();
+    }
+
+    @Then("my shopping cart should contain {int} item")
+    public void my_shopping_cart_should_contain_item(int int1) {
+        sut.getCartAmount();
+        sut.cartHasOneItem(int1);
+    }
+
+
+
+    @When("I add {int} products to my shopping cart")
+    public void i_add_num_products_products_to_my_shopping_cart(int int1) {
+        sut.selectMultipleProductsAndAddToCart(int1);
+    }
+
+    @Then("my shopping cart should contain {int} items")
+    public void my_shopping_cart_should_contain_num_products_items(int int1) {
+        sut.getCartAmount();
+        sut.cartHasMultipleItems(int1);
+    }
+
+}
